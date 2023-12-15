fdcbb66f01116aa9a71b3ac06dde0bb832a0f0e8
Hugo THOMAS <thomas.hugo18@sfr.fr>
2023-12-15 09:03:27 +0100

    Add readme

---
+++ src/README.md
@@ -0,0 +1,24 @@
+# teensy-simple-synth
+
+Simple polyphonic synthetizer for the teensy microcontroller, using a modular architecture.
+It also includes a configurable arpeggiator.
+The teensy USB type must be configured as Serial + MIDI.
+
+To configure the synthetizer while it is running, send commands via the Serial interface.
+
+## Configure a new voice sound
+Add an if-else block in the createVoices function and choose the waveform and envelope. Increment the `VOICE_SOUND_NB` define constant.
+
+To change the synthetizer voice sound while it is running, use:
+- `voice <v>` --> change the voice sound to v, v must be a valid int between 0 and VOICE_SOUND_NB
+
+## Arpeggiator related commands
+- `arp on` --> enable the arpeggiator
+- `arp off` --> disable the arpeggiator
+- `arp mode <m>` --> change the arpeggiator mode, m can be:
+    - `up` --> play notes from bottom to top
+    - `down` --> play notes from top to bottom
+    - `updown` --> play notes from bottom to top then top to bottom
+    - `downup` --> play notes from top to bottom then bottom to top
+- `arp note <d>` --> configure the note duration in seconds, d must be a valid float
+- `arp inter <d>` --> configure the note interval duration in seconds, d must be a valid float
--- teensy-simple-synth.ino
+++ teensy-simple-synth.ino
@@ -10,6 +10,8 @@
 #include "src/envelopes/ADSREnvelope.h"
 #include "src/waveforms/Triangle.h"
 
+#define VOICE_SOUND_NB 1
+
 Synth synth;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
@@ -17,11 +19,11 @@ AudioConnection patchCord0(synth, 0, out, 0);
 AudioConnection patchCord1(synth, 0, out, 1);
 bool arpeggiatorOn;
 
-std::unique_ptr<std::vector<Voice>> createVoices() {
+std::unique_ptr<std::vector<Voice>> createVoices(int voiceSound) {
     auto voices = std::make_unique<std::vector<Voice>>();
 
     for (auto i = 0; i < VOICE_NB; i++) {
-        if (true) {
+        if (voiceSound == 0) {
             auto waveform = std::make_unique<Triangle>(AUDIO_SAMPLE_RATE_EXACT);
             auto envelope = std::make_unique<ADSREnvelope>(AUDIO_SAMPLE_RATE_EXACT, 0.2, 0.3, 0.8, 2.0, 0.3, 0.0001);
             voices->push_back(Voice(AUDIO_SAMPLE_RATE_EXACT, std::move(waveform), std::move(envelope), synth));
@@ -59,7 +61,14 @@ void onControlChange(byte channel, byte control, byte value) {
 }
 
 void processInput(String input) {
-    if (input.startsWith("arp ")) {
+    if (input.startsWith("voice ")) {
+        int voiceSound = input.substring(6).toInt();
+        if (voiceSound < VOICE_SOUND_NB && voiceSound >= 0) {
+            synth.setVoices(createVoices(voiceSound));
+            Serial.print("Switched to voice sound ");
+            Serial.println(voiceSound);
+        }
+    } else if (input.startsWith("arp ")) {
         String s = input.substring(4);
         if (s.startsWith("on")) {
             arpeggiatorOn = true;
@@ -103,7 +112,7 @@ void processInput(String input) {
 }
 
 void setup() {
-    synth.setVoices(createVoices());
+    synth.setVoices(createVoices(0));
     arpeggiatorOn = false;
 
     Serial.begin(9600);
